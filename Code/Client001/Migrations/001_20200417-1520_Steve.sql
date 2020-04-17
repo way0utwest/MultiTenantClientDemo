37cf0ca4087e8d0ba06c2df7d27ca724ab5b1abf
way0utwest <sjones@sqlservercentral.com>
2020-04-17 15:20:44 -0600

    New table for custom supplier information

---
+++ Code/Client001/Migrations/001_20200417-1520_Steve.sql
@@ -0,0 +1,11 @@
+﻿-- <Migration ID="54491bff-3ec4-4186-a387-80b7507c8711" />
+GO
+
+PRINT N'Creating [dbo].[Supplier_Client001]'
+GO
+CREATE TABLE [dbo].[Supplier_Client001]
+(
+[SupplierKey] [int] NOT NULL IDENTITY(1, 1),
+[SupplierName] [varchar] (100) NULL
+)
+GO
---
+++ Code/Client001/Schema-Model/dbo/Tables/Supplier_Client001.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Supplier_Client001]
+(
+[SupplierKey] [int] NOT NULL IDENTITY(1, 1),
+[SupplierName] [varchar] (100) NULL
+)
+GO
