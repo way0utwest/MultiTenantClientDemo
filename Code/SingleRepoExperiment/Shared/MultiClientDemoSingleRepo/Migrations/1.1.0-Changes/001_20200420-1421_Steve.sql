cab31d5fc00c69c4cfa4d0d73a30f0a703724aa4
way0utwest <sjones@sqlservercentral.com>
2020-04-20 14:23:51 -0600

    Added client change to track date created

---
+++ Code/SingleRepoExperiment/Shared/MultiClientDemoSingleRepo/Migrations/1.1.0-Changes/001_20200420-1421_Steve.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="facd0d74-0149-4b59-beb3-e97385afcbbc" />
+GO
+
+PRINT N'Altering [dbo].[Client]'
+GO
+ALTER TABLE [dbo].[Client] ADD
+[DateCreated] [datetime2] (3) NULL CONSTRAINT [DF__Client__DateCrea__45F365D3] DEFAULT (sysutcdatetime())
+GO
--- Code/SingleRepoExperiment/Shared/MultiClientDemoSingleRepo/Schema-Model/dbo/Tables/Client.sql
+++ Code/SingleRepoExperiment/Shared/MultiClientDemoSingleRepo/Schema-Model/dbo/Tables/Client.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[Client]
 (
 [ClientID] [int] NOT NULL IDENTITY(1, 1),
 [ClientName] [varchar] (100) NULL,
-[Active] [tinyint] NULL
+[Active] [tinyint] NULL,
+[DateCreated] [datetime2] (3) NULL CONSTRAINT [DF__Client__DateCrea__45F365D3] DEFAULT (sysutcdatetime())
 )
 GO
 ALTER TABLE [dbo].[Client] ADD CONSTRAINT [ClientPK] PRIMARY KEY CLUSTERED  ([ClientID])
